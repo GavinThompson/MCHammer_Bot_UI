ecaad7af209df0d9312dc6276ed0a99bf8ab6208
Gavin Thompson <gavinathompson@gmail.com>
2013-06-20 00:38:35 -0400

    Deleted previous files for robot functionality

--- test.sh
+++
@@ -1,5 +0,0 @@
-curl -X POST http://localhost:8071/motion-control
-
-curl -X POST -d "strafe=1" http://localhost:8071/motion-control
-
-curl -X POST -d "strafe=0.1&forward=0.2" http://localhost:8071/motion-control
